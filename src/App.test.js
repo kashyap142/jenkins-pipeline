e8f3ca2c32fe3fc58c1a83c5b37f8980df45af21
Kunal Kashyap <kashyapkunal142@gmail.com>
2024-06-17 10:49:26 +0530

    prettier configure

--- src/App.test.js
+++ src/App.test.js
@@ -4,17 +4,17 @@ import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
 
 test("renders App component without crashing", () => {
-    render(<App />);
+  render(<App />);
 });
 
 test('contains an h2 element with the text "Hello World!!"', () => {
-    render(<App />);
-    const headingElement = screen.getByText(/Hello World!!/i);
-    expect(headingElement).toBeInTheDocument();
+  render(<App />);
+  const headingElement = screen.getByText(/Hello World!!/i);
+  expect(headingElement).toBeInTheDocument();
 });
 
 test('contains a p element with the text "This is a paragraph"', () => {
-    render(<App />);
-    const paragraphElement = screen.getByText(/This is a paragraph/i);
-    expect(paragraphElement).toBeInTheDocument();
+  render(<App />);
+  const paragraphElement = screen.getByText(/This is a paragraph/i);
+  expect(paragraphElement).toBeInTheDocument();
 });
