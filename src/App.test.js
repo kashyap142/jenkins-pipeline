adf2e362f35b32075f2611f826a78eaa73f737ae
Kunal Kashyap <kashyapkunal142@gmail.com>
2024-06-17 11:10:42 +0530

    fixed test

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,16 @@
+// src/App.test.js
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
 
-test("renders App component without crashing", () => {
+test("renders app component", () => {
   render(<App />);
-});
 
-test('contains an h2 element with the text "Hello World!!"', () => {
-  render(<App />);
+  // Check if the text "Hello World!!" is present
   const headingElement = screen.getByText(/Hello World!!/i);
   expect(headingElement).toBeInTheDocument();
-});
 
-test('contains a p element with the text "This is a paragraph"', () => {
-  render(<App />);
+  // Check if the text "This is a paragraph" is present
   const paragraphElement = screen.getByText(/This is a paragraph/i);
   expect(paragraphElement).toBeInTheDocument();
 });
