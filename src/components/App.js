3fba677414a58e71ed64ba7c7352e60125a3fdc0
Kunal Kashyap <kashyapkunal142@gmail.com>
2024-06-17 19:10:07 +0530

    add input field

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import Input from "./Input";
 function App() {
   return (
     <>
       <Header />
       <h2>Hello World!!</h2>
       <p>This is a paragraph</p>
+
+      <Input />
 
       <Footer />
     </>
