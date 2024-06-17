3a24a6fa502136848238e94e74e16523c49f1531
Kunal Kashyap <kashyapkunal142@gmail.com>
2024-06-17 10:32:38 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div className="App">
+            <h2>Hello World!!</h2>
+            <p>This is a paragraph</p>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import App from "./App";
+
+test("renders App component without crashing", () => {
+    render(<App />);
+});
+
+test('contains an h2 element with the text "Hello World!!"', () => {
+    render(<App />);
+    const headingElement = screen.getByText(/Hello World!!/i);
+    expect(headingElement).toBeInTheDocument();
+});
+
+test('contains a p element with the text "This is a paragraph"', () => {
+    render(<App />);
+    const paragraphElement = screen.getByText(/This is a paragraph/i);
+    expect(paragraphElement).toBeInTheDocument();
+});
